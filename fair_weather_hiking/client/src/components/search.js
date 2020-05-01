3eaa6b91c890a276f66340d09920024ebc2f1dc0
Sarah Carter <sarahmarie.carter@yahoo.com>
2020-05-01 16:52:43 -0700

    added a search button to search component with onSubmit event handler

--- fair_weather_hiking/client/src/components/search.js
+++ fair_weather_hiking/client/src/components/search.js
@@ -1,11 +1,40 @@
-import React from 'react';
+import React, { Component } from 'react';
 
-function Search() {
-    return(
-        <h4>Welcome to the search form page</h4>
-        //create form here that allows user to search for hikes within radius
-        //on submit, take user to results page
-    )
+class Search extends Component {
+
+    onSubmit(event) {
+        event.preventDefault();
+        //api call to get hikes with criteria
+        console.log('searched for a hike')
+    }
+
+    render() {
+        return(
+            <div className='search-form'>
+                <h4>Welcome to the search form page</h4>
+                <form onSubmit={this.onSubmit}>
+                
+                    <button
+                    style={{
+                        width: "150px",
+                        borderRadius: "3px",
+                        letterSpacing: "1.5px",
+                        marginTop: "1rem"
+                    }}
+                    type="submit"
+                    className="btn btn-large waves-effect waves-light hoverable blue accent-3"
+                    >
+                    Search
+                    </button>
+                    
+                </form>
+            </div>
+    
+            //create form here that allows user to search for hikes within radius
+            //on submit, take user to results page
+        )        
+    }
+    
 }
 
 export default Search;
