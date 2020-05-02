81cdecbd8eb93796bbce857ac7bf8b4aa2976803
Sarah Carter <sarahmarie.carter@yahoo.com>
2020-05-01 22:20:59 -0700

    changed max distance and max elevation from input fields to Materialize dropdowns

--- fair_weather_hiking/client/src/components/search.js
+++ fair_weather_hiking/client/src/components/search.js
@@ -2,7 +2,19 @@ import React, { Component } from 'react';
 import Button from './button';
 import Label from './label';
 
+import M from 'materialize-css';
+
+// document.addEventListener('DOMContentLoaded', function() {
+//     var elems = document.querySelectorAll('select');
+//     var instances = M.FormSelect.init(elems, options);
+//   });
+
 class Search extends Component {
+
+    //initialize Materialize
+    componentDidMount() {
+        M.AutoInit();
+    }
 
     constructor() {
         super();
@@ -14,17 +26,18 @@ class Search extends Component {
 
     onChange = event => {
         //console.log(event.target.value);
-        this.setState({ [event.target.id]: event.target.value })
-        //console.log(event.target.id)
+        //this.setState({ [event.target.id]: event.target.value })
     }
 
     onSubmit(event) {
         event.preventDefault();
-        const data = {
-            maxDistance: this.state.maxDistance,
-            maxElevation: this.state.maxElevation
-        }
-        console.log('data: ' + data);
+        console.log(this);
+
+        // const data = {
+        //     maxDistance: this.state.maxDistance,
+        //     maxElevation: this.state.maxElevation
+        // }
+
         //const query = 
         //this.props.searchHikes(query)
         //api call to get hikes with criteria
@@ -32,12 +45,12 @@ class Search extends Component {
     }
 
     render() {
+        console.log(this.state)
         return(
-            <div className='container'>
-                <h4>Welcome to the search form page</h4>
+            <div className='container search'>
                 <div className='row'>
                     <div className='col s8 offset-s2'>
-                        <form noValidate onSubmit={this.onSubmit}>
+                        {/* <form noValidate onSubmit={this.onSubmit}>
                             <div className='input-field col s12'>
                                 <input
                                     onChange={this.onChange}
@@ -54,6 +67,26 @@ class Search extends Component {
                             </div>
                             <br />
                             <Button name='Search' />
+                        </form> */}
+                        <form noValidate onSubmit={this.onSubmit}>
+                            <div className="input-field col s12">
+                                <select>
+                                    <option value="" disabled selected>Select Max Distance</option>
+                                    <option value="1">1 mile</option>
+                                    <option value="2">2 mile</option>
+                                    <option value="3">3 mile</option>
+                                </select>
+                            </div>
+                            <div className="input-field col s12">
+                                <select>
+                                    <option value="" disabled selected>Select Max Elevation Gain</option>
+                                    <option value="1">1000 ft</option>
+                                    <option value="2">2000 ft</option>
+                                    <option value="3">3000 ft</option>
+                                </select>
+                            </div>
+                            <br />
+                            <Button name='Search' type='submit' />
                         </form>
                     </div>
                 </div>
