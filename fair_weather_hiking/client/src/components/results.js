dae8e9b2d29fdc5fa6425989ca0e515b51d5153b
doctorbebo <byronrb92@gmail.com>
2020-05-02 14:05:11 -0700

    made index card. Also added public assets folder with images

--- fair_weather_hiking/client/src/components/results.js
+++ fair_weather_hiking/client/src/components/results.js
@@ -1,8 +1,26 @@
 import React from 'react';
+import HikeCard from './results/hike_card';
 
 function Results() {
+
+    // Fake hike data for testing replace with real data. 
+    const hikeData = {
+        name: "Hike Name",
+        difficulty: "Test Easy",
+        distance: 100, 
+        elevation: 1000,
+        imageURL: "https://images.unsplash.com/photo-1551632811-561732d1e306?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1050&q=80", 
+        weather: "clear",
+        summary: "This is a summary of the hike with details about the hike. It will include details the hike. Is the hike in the forest? Maybe it's by a beach?"
+    }
+
+
     return(
-        <h2>Welcome to the results page</h2>
+        <div>
+            <h2>Welcome to the results page</h2>
+            <HikeCard hikeData = {hikeData} />;
+        </div>
+
     )
 }
 
