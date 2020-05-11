ea48aa9bee81355b3d141af1113d7fa8fe4becc0
Sarah Carter <sarahmarie.carter@yahoo.com>
2020-05-10 20:18:57 -0700

    added favorite hike cards to a favorite container that is 6 cols wide, offset 2

--- fair_weather_hiking/client/src/components/favorites/favoriteContainer.js
+++ fair_weather_hiking/client/src/components/favorites/favoriteContainer.js
@@ -1,51 +1,12 @@
-import React, { Component } from 'react';
-import PropTypes from "prop-types";
-import { connect } from "react-redux";
-import axios from 'axios';
+import React from 'react';
+import Favorites from './favoriteCards';
 
-import HikeCard from '../results/hike_card';
-import './style.css';
-
-class Favorites extends Component {
-    constructor() {
-        super();
-        this.state = {
-            favorites: []
-        }
-    }
-
-    componentDidMount() {
-        let id = this.props.auth.user.id
-        axios.get(`/api/users/favorite/${id}`)
-        .then(res => {
-            console.log(res.data)
-            this.setState({
-                favorites: res.data
-            })
-        })
-    }
-
-    render() {
-        return this.state.favorites.map(trail => {
-            return <HikeCard id={trail.id}
-            name={trail.name}
-            difficulty={trail.difficulty}
-            elevation={trail.elevation}
-            imageURL={trail.imageURL}
-            weather='clear'
-            summary={trail.summary} />
-        })
-    }
+function FavoriteContainer() {
+    return <div className='row'>
+        <div className='col s6 offset-s3'>
+            <Favorites />
+        </div>
+    </div>
 }
 
-Favorites.propTypes = {
-    auth: PropTypes.object.isRequired
-  };
-  
-  const mapStateToProps = state => ({
-    auth: state.auth
-  });
-  
-  export default connect(
-    mapStateToProps
-  )(Favorites);
+export default FavoriteContainer;
