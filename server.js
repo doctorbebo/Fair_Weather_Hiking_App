5b86a115e590764b61763d69015806845f201c85
doctorbebo <byronrb92@gmail.com>
2020-05-11 20:31:58 -0700

    changed to online database

--- server.js
+++ server.js
@@ -25,10 +25,13 @@ app.use(bodyParser.json());
 // DB Config
 const db = require("./config/keys").mongoURI;
 // Connect to MongoDB
-mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/fairweather", { useNewUrlParser: true })
+mongoose
+  .connect(
+    db,
+    { useNewUrlParser: true }
+  )
   .then(() => console.log("MongoDB successfully connected"))
   .catch(err => console.log(err));
-
 // Passport middleware
 app.use(passport.initialize());
 
