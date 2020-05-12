1045534fac0bff49e85b4cd838b5981f42108c64
doctorbebo <byronrb92@gmail.com>
2020-05-11 20:06:19 -0700

    added require path

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ const express = require('express');
 const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
 const passport = require('passport');
+const path = require('path');
 
 const users = require('./routes/api/users');
 const hikes = require('./routes/api/hikes')
