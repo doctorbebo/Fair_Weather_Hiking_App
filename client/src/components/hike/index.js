9b11bceb3d48056b0335ec81ca0a03e18889fc54
Sarah Carter <sarahmarie.carter@yahoo.com>
2020-05-26 20:03:35 -0700

    added some styling to hike card more info

--- client/src/components/hike/index.js
+++ client/src/components/hike/index.js
@@ -1,7 +1,6 @@
 import React, { Component } from "react";
 import WeatherCard from "./weatherCard/weatherCard"
 import BestDay from "./BestDay/BestDay"
-import Reports from "./reports/reports"
 import Summary from "./Summary/summary"
 
 
@@ -11,31 +10,29 @@ class Hike extends Component {
         <div >
             <div className="row">
                 <div className="col l12 m12 s12 center-align">
-                    <h6>Summary:</h6>
-                    <div className = "row">
-                      <Summary 
-                        summary = {this.props.summary}
-                      ></Summary>
-                  </div>   
-                  <h6>5-Day Forcast:</h6>
+                    <div className='summary'>
+                      <h6>Summary:</h6>
+                      <div className = "row">
+                        <Summary 
+                          summary = {this.props.summary}
+                        ></Summary>
+                      </div>
+                    </div>
+                  <h6>5-Day Forecast:</h6>
                   <div className = "row">
                       <div className = "col s1 m1 12"></div>
                       <WeatherCard forecast = {this.props.forecast}></WeatherCard>
 
                   </div>
-                  <h6>Best Day:</h6>
-                  <div className = "row">
-                      <BestDay 
-                        bestDay = {this.props.bestDay}
-                      ></BestDay>
+                  <div className='summary'>
+                    <h6>Best Day:</h6>
+                    <div className = "row">
+                        <BestDay 
+                          bestDay = {this.props.bestDay}
+                        ></BestDay>
+                    </div>
                   </div>
-                  <h6>Reports:</h6>
-                  <div className = "row">
-                      <Reports 
-                
-                      ></Reports>
-                  </div>
-
+                  <br />
               </div>
           </div>
       </div>
